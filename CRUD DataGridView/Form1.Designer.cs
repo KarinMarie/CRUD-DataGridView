4478464542e9b6caaed26872a367a8632bb4dc51
Karin Marie <93265472+KarinMarie@users.noreply.github.com>
2022-07-31 16:50:27 -0300

    Add files via upload

---
+++ CRUD DataGridView/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace CRUD_DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.databaseDataSet = new CRUD_DataGridView.DatabaseDataSet();
+            this.pessoaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pessoaTableAdapter = new CRUD_DataGridView.DatabaseDataSetTableAdapters.PessoaTableAdapter();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pessoaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(619, 397);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(178, 41);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "UPDATE RECORDS";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(435, 397);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(178, 41);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "SHOW RECORDS";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // databaseDataSet
+            // 
+            this.databaseDataSet.DataSetName = "DatabaseDataSet";
+            this.databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pessoaBindingSource
+            // 
+            this.pessoaBindingSource.DataMember = "Pessoa";
+            this.pessoaBindingSource.DataSource = this.databaseDataSet;
+            // 
+            // pessoaTableAdapter
+            // 
+            this.pessoaTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(3, 6);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(794, 385);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 445);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnUpdate);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD DataGridView";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pessoaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnShow;
+        private DatabaseDataSet databaseDataSet;
+        private System.Windows.Forms.BindingSource pessoaBindingSource;
+        private DatabaseDataSetTableAdapters.PessoaTableAdapter pessoaTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ CRUD DataGridView/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUD_DataGridView
+{
+    public partial class Form1 : Form
+    {
+        SqlDataAdapter sda;
+        SqlCommandBuilder scb;
+        DataTable dt;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\Karin Marie\Desktop\Repositorios\CRUD DataGridView\CRUD DataGridView\Database.mdf';Integrated Security=True");
+            sda = new SqlDataAdapter("SELECT * FROM Pessoa", con);
+
+            dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView.DataSource = dt;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            scb = new SqlCommandBuilder(sda);
+            sda.Update(dt);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'databaseDataSet.Pessoa'. Você pode movê-la ou removê-la conforme necessário.
+            this.pessoaTableAdapter.Fill(this.databaseDataSet.Pessoa);
+
+        }
+    }
+}
